ebe86a42eecabddd397161b7d84147cd5b8f5884
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-09-02 17:26:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function YDM() {
- alert("It is a metre guage locomotive. They entered the service in 1955.A total of 20 YDM-1 locomotives was built between 1954 and 1956. Max Speed - 83km/h Now these rare locomotives are preserved in museums."); 
+ alert("This is YDM 1 locomotive. It is a metre guage locomotive. They entered the service in 1955.A total of 20 YDM-1 locomotives was built between 1954 and 1956. Max Speed - 83km/h Now these rare locomotives are preserved in museums."); 
 }
 
 function WDM() {
