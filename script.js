50cf9a72a42f18a285d7be06ad3105063f941ecf
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-09-02 17:30:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function YDM() {
- alert("This is YDM 1 locomotive. It is a metre guage locomotive. They entered the service in 1955.A total of 20 YDM-1 locomotives was built between 1954 and 1956. Max Speed - 83km/h Now these rare locomotives are preserved in museums."); 
+ alert("This is YDM 1 locomotive. It is a metre guage locomotive. They entered the service in 1955. A total of 20 YDM-1 locomotives was built between 1954 and 1956. Max Speed - 83km/h. These locomotives were withdrawn from the service. Now these rare locomotives are preserved in museums."); 
 }
 
 function WDM() {
- alert(""); 
+ alert("This is WDm 1 locomotive. It is a broad gauge locomotive. Max speed - 100km/h, power output - 1800hp. One locomotive is still preserved in the National rail museum in New Delhi while the others have been withdrawn from the service"); 
 }
 
 function WDMA() {
